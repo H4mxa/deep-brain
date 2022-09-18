f5185a0ab90c02986d92f89ba17de37ef5280fb1
h4mxa <duke80800@gmail.com>
2022-09-17 18:18:18 -0700

    optimize spacer

--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -5,22 +5,29 @@ import {
   Info,
   Title,
   Address,
+  Section,
   Rating,
+  SectionEnd,
+  CloseMessage,
+  LodgeIcon,
 } from './styles';
 import {SvgXml} from 'react-native-svg';
 import star from '../../../../assets/star';
+import open from '../../../../assets/open';
+import {Spacer} from '../../../components/spacer/spacer.component';
 
 const RetaurantInfoCard = ({restaurant = {}}) => {
   const {
     name = 'Some Restaurant',
-    icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = [
       'https://images.squarespace-cdn.com/content/v1/5804a264e4fcb5e6ae7003cc/1486548396592-Y2IIF52HNW0VRHVQZYHE/url.jpeg',
     ],
     address = '100 some random street',
     openingHours = true,
+    isOpenNow = true,
     rating = 3,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -30,11 +37,24 @@ const RetaurantInfoCard = ({restaurant = {}}) => {
       <RestaurantCardCover key={name} source={{uri: photos[0]}} />
       <Info>
         <Title>Card content</Title>
-        <Rating>
-          {ratingArray.map(() => (
-            <SvgXml xml={star} width={20} height={20} />
-          ))}
-        </Rating>
+        <Section>
+          <Rating>
+            {ratingArray.map((ele, idx) => (
+              <SvgXml key={idx} xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <CloseMessage variant="label">CLOSED TEMPORARILY</CloseMessage>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <LodgeIcon source={{uri: icon}} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
         <Address>100 some random street</Address>
       </Info>
     </RestaurantCard>
--- src/features/restaurants/components/styles.js
+++ src/features/restaurants/components/styles.js
@@ -1,6 +1,6 @@
-import {View} from 'react-native';
 import styled from 'styled-components/native';
 import {Card} from 'react-native-paper';
+import {SvgXml} from 'react-native-svg';
 
 export const RestaurantCard = styled(Card)`
   background-color: ${props => props.theme.colors.bg.primary};
@@ -13,10 +13,29 @@ export const Info = styled.View`
   padding: ${props => props.theme.space[3]};
 `;
 
+export const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
 export const Rating = styled.View`
   flex-direction: row;
   padding-top: ${props => props.theme.space[2]};
   padding-bottom: ${props => props.theme.space[2]};
+  justify-content: space-between;
+`;
+
+export const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+export const CloseMessage = styled.Text`
+  color: 'red';
+`;
+export const LodgeIcon = styled.Image`
+  width: 15px;
+  height: 15px;
 `;
 
 export const Title = styled.Text`
